a15ba6e002fe1dc6ac8ec13032213fca8c71dbfb
Sindhuja0804 <31072499+Sindhuja0804@users.noreply.github.com>
2017-08-16 22:52:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleone', function (req, res) {
+  res.send("articleo one");
+});
+
+app.get('/articltwo', function (req, res) {
+  res.send("articleo 2");
+});
+
+app.get('/articlthree', function (req, res) {
+  res.send("articleo 3");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
